1ed4b6c488c0003bf553c6bcf3ad3f331c714554
Benoit Asselin <contact@ab-d.fr>
2015-05-05 09:54:24 +0200

    Create ucopia.bat

---
+++ ucopia.bat
@@ -0,0 +1,5 @@
+REM http://curl.haxx.se/download.html
+:ucopia
+CURL.EXE --header "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0" --data "action=authenticate&login=LOGIN&password=PASSWORD&policy_accept=false" https://controller.mobile.lan/portal_api.php
+timeout /t 600 /nobreak
+goto :ucopia
