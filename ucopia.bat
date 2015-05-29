e6810195037ee3b5a80d1959041883e3e7fbde76
Benoit Asselin <contact@ab-d.fr>
2015-05-29 10:09:25 +0200

    Update ucopia.bat

--- ucopia.bat
+++ ucopia.bat
@@ -1,5 +1,30 @@
-REM http://curl.haxx.se/download.html
+@echo off
+REM Telechargez http://curl.haxx.se/download.html
+
+REM Veuillez saisir votre identifiant et votre mot de passe ci-dessous
+set login=mon_identifiant
+set password=mon_mot_de_passe
+
+REM Les API possible
+set api=https://controller.access.network/portal_api.php
+REM set api=https://controller.mobile.lan/portal_api.php
+
 :ucopia
-CURL.EXE --header "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0" --data "action=authenticate&login=LOGIN&password=PASSWORD&policy_accept=false" https://controller.access.network/portal_api.php
-timeout /t 600 /nobreak
+	for /f "delims=" %%i in ('CURL.EXE --header "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0" --data "action=authenticate&login=%login%&password=%password%&policy_accept=false" %api%') do set curlOutput=%%i
+	echo %curlOutput% > curl.log
+	findstr "CONNECT" curl.log >NUL
+	if %errorlevel% neq 0 goto :curlError
+
+	del curl.log
+	echo.
+	echo Vous etes connecte!
+	timeout /t 600 /nobreak
+goto :ucopia
+
+:curlError
+	echo.
+	echo %curlOutput%
+	echo.
+	echo La connexion a echoue...
+	timeout /t 6
 goto :ucopia
